fddad7093859ec83b987e1cfc3fa3909da868135
aemem <anamoliner.marques@gmail.com>
2022-04-26 22:15:35 +0000

    primero

--- src/models.py
+++ src/models.py
@@ -8,25 +8,50 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class characters(Base):
+     __tablename__ = 'characters'
+     id = Column(Integer, primary_key=True)
+     name = Column(String(250), nullable=False)
+     age = Column(Integer, nullable=False)
+     hair_color = Column(String(250), nullable=False)
+     eye_color = Column(String(250), nullable=False)
+
+class planets(Base):
+     __tablename__ = 'planets'
+     id = Column(Integer, primary_key=True)
+     name = Column(String(250), nullable=False)
+     population = Column(Integer, nullable=False)
+     climate = Column(String(250), nullable=False)
+     terrain = Column(String(250), nullable=False)
+
+class vehicles(Base):
+     __tablename__ = 'vehicles'
+     id = Column(Integer, primary_key=True)
+     name = Column(String(250), nullable=False)
+     model = Column(String(250), nullable=False)
+     cargo_capacity = Column(Integer, nullable=False)
+     crew = Column(Integer, nullable=False)
+
+class favorites(Base):
+    __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
+    characters_id = Column(Integer, ForeignKey('characters.id'))
+    characters = relationship(characters)
+    planets_id = Column(Integer, ForeignKey('planets.id'))
+    planets = relationship(characters)
+    vehicles_id = Column(Integer, ForeignKey('vehicles.id'))
+    vehicles = relationship(vehicles)
+
+class user(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(250), nullable=False, unique=True)
     name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    favorites_id = Column(Integer, ForeignKey('favorites.id'))
+    favorites = relationship(favorites)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
+def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
